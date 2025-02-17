acd61cfe4ffd69a152815db867438727fd6ab0c3
Oscar Chavez <42700726+ocb-dev-04@users.noreply.github.com>
2025-02-17 15:01:51 -0400

    Create IdempotentCommand.cs

---
+++ CQRS.MediatR.Helper/Abstractions/Messaging/IdempotentCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace CQRS.MediatR.Helper.Abstractions.Messaging;
+
+/// <summary>
+/// An <see cref="IdempotentCommand"/> to use when endpoint doesn't return value
+/// </summary>
+public interface IdempotentCommand 
+    : ICommand;
+
+/// <summary>
+/// An <see cref="IdempotentCommand{TResponse}"/> to use when endpoint return value
+/// </summary>
+/// <typeparam name="TResponse"></typeparam>
+public interface IdempotentCommand<TResponse> 
+    : ICommand<TResponse>;
