c89ae9ee607b71ebabc23746bdde5276cd10ee24
Oscar Chavez <oscarchb04@gmail.com>
2025-04-19 16:26:56 -0400

    Update IdempotentCommand.cs

--- CQRS.MediatR.Helper/Abstractions/Messaging/IdempotentCommand.cs
+++ CQRS.MediatR.Helper/Abstractions/Messaging/IdempotentCommand.cs
@@ -3,12 +3,12 @@
 /// <summary>
 /// An <see cref="IdempotentCommand"/> to use when endpoint doesn't return value
 /// </summary>
-public abstract record IdempotentCommand(Guid RequestId)
+public abstract record IdempotentCommand()
     : ICommand;
 
 /// <summary>
 /// An <see cref="IdempotentCommand{TResponse}"/> to use when endpoint return value
 /// </summary>
 /// <typeparam name="TResponse"></typeparam>
-public abstract record IdempotentCommand<TResponse>(Guid RequestId)
+public abstract record IdempotentCommand<TResponse>()
     : ICommand<TResponse>;
