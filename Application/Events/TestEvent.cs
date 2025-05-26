33633c03a4c988d551c8b0006bc28e5a22f6efaf
Oscar Chavez <oscarchb04@gmail.com>
2025-05-25 22:20:28 -0400

    *Add INotification, INotificationHandler and dispatcher

---
+++ Application/Events/TestEvent.cs
@@ -0,0 +1,5 @@
+﻿using OCB.Mediator.Helper.Abstractions.Notification;
+
+namespace Application.Events;
+
+internal sealed record TestEvent(string Name, string Description) : INotification;
---
+++ OCB.Mediator.Helper/Abstractions/Notification/INotification.cs
@@ -0,0 +1,6 @@
+﻿namespace OCB.Mediator.Helper.Abstractions.Notification;
+
+/// <summary>
+/// Interface to use for notifications.
+/// </summary>
+public interface INotification;
