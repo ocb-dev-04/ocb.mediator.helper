4680473fd869572d368252d2c1e2385f5ab3243c
Oscar Chavez <oscarchb04@gmail.com>
2025-08-24 17:02:17 -0400

    *Move to controller to test Match method

---
+++ Web.Api/Controllers/BaseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OCB.Mediator.Helper.ResultPattern;
+using OCB.Mediator.Helper.Abstractions.Sender;
+
+namespace Web.Api.Controllers;
+
+/// <summary>
+/// Base controller for all controllers in the application.
+/// </summary>
+public class BaseController : ControllerBase
+{
+    protected readonly IHttpContextAccessor _httpContextAccessor;
+    protected readonly ISender _sender;
+
+    /// <summary>
+    /// <see cref="BaseController"/> public constructor.
+    /// </summary>
+    /// <param name="httpContextAccessor"></param>
+    /// <param name="sender"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    protected BaseController(
+        IHttpContextAccessor httpContextAccessor,
+        ISender sender)
+    {
+        _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+    }
+
+    /// <summary>
+    /// Handles error results based on the provided <see cref="Error"/> object.
+    /// </summary>
+    /// <param name="error"></param>
+    /// <returns></returns>
+    protected IActionResult HandleErrorResults(Error error)
+        => error.StatusCode switch
+        {
+            304 => StatusCode(error.StatusCode, new { error.Translation, error.Description }),
+            400 => BadRequest(new { error.Translation, error.Description }),
+            401 => Unauthorized(),
+            404 => NotFound(new { error.Translation, error.Description }),
+            _ => StatusCode(error.StatusCode)
+        };
+}
---
+++ Web.Api/Controllers/TodosController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.UsesCases.Create;
+using Application.UsesCases.Delete;
+using Application.UsesCases.GetById;
+using OCB.Mediator.Helper.Extensions;
+using OCB.Mediator.Helper.ResultPattern;
+using OCB.Mediator.Helper.Abstractions.Sender;
+
+namespace Web.Api.Controllers;
+
+[ApiController]
+[Route("api/todos")]
+[Produces("application/json")]
+public sealed class TodosController : BaseController
+{
+    public TodosController(
+        IHttpContextAccessor httpContextAccessor, 
+        ISender sender) : base(httpContextAccessor, sender)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetById(
+        Guid id,
+        CancellationToken cancellationToken)
+    {
+        GetByIdQuery query = new GetByIdQuery(id);
+        Result<GetByIdResponse> result = await _sender.Send(query, cancellationToken);
+
+        return result.Match(Ok, HandleErrorResults);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        CreateRequest request,
+        CancellationToken cancellationToken)
+    {
+        CreateCommand command = new(request.Name, request.Description);
+        Result<Guid> result = await _sender.Send(command, cancellationToken);
+
+        return result.Match(
+            value => Created(string.Empty, value),
+            HandleErrorResults);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(
+        Guid id,
+        CancellationToken cancellationToken)
+    {
+        DeleteCommand command = new(id);
+        Result result = await _sender.Send(command, cancellationToken);
+
+        return result.Match(Ok, HandleErrorResults);
+    }
+}
+
+/// <summary>
+/// Represents a request to create a new entity with a name and description.
+/// </summary>
+/// <remarks>This record is typically used to encapsulate the data required for creating a new entity. Both <see
+/// cref="Name"/> and <see cref="Description"/> are required to provide meaningful context for the entity being
+/// created.</remarks>
+/// <param name="Name"></param>
+/// <param name="Description"></param>
+public record CreateRequest(string Name, string Description);
